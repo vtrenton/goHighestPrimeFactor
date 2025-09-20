578f5fbbfb729fe1b775e7cda23bd8d6d14ebe9d
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-09-20 16:10:53 -0500

    oops wrong variable - fixed bug
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- hpf.go
+++ hpf.go
@@ -56,7 +56,7 @@ func relevantFactors(n int) []int {
 
 	limit := int(math.Sqrt(float64(n)))
 	for i := 2; i <= limit; i++ {
-		if n > 2 && n%2 == 0 { // even numbers above 2 are never prime
+		if i > 2 && i%2 == 0 { // even numbers above 2 are never prime
 			continue
 		}
 		if n%i == 0 {
