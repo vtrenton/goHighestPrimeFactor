e4def838d5c3e41231d5fe5ee8a14ad6ac484077
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-09-20 13:28:35 -0500

    got rid of old OS exit method is run()
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- hpf.go
+++ hpf.go
@@ -24,7 +24,6 @@ func main() {
 func run(osargs []string) (int, error) {
 	if len(osargs) != 2 {
 		return 0, errors.New("usage: pass in a single  postive number as an Arg that you want the highest prime factorial for")
-		os.Exit(1)
 	}
 
 	n, err := strconv.Atoi(osargs[1])
